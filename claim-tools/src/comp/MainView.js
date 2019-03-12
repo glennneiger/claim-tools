08fef2ce2411179c39b4f4ff65f31cb2ab2697cd
Matt Peters <matthew.george.peters@gmail.com>
2019-03-12 19:30:15 -0400

    added NewClaim comp

--- claim-tools/src/comp/MainView.js
+++ claim-tools/src/comp/MainView.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import NewClaim from './NewClaim';
 
 class MainView extends Component {
+    state ={
+
+    }
+
     render() {
         return (
             <section className='mainview'>
                 <h1>Main Section</h1>
+                <NewClaim />
             </section>
         );
     }
---
+++ claim-tools/src/comp/NewClaim.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewClaim extends Component {
+    render() {
+        return (
+            <section className='newclaim'>
+                <h1>new claim</h1>
+            </section>
+        );
+    }
+}
+
+export default NewClaim;
