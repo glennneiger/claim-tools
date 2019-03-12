60dbc82fe4f5776c961940bcd489e00b5d7e61ca
Matt Peters <matthew.george.peters@gmail.com>
2019-03-12 18:56:00 -0400

    added Header comp

---
+++ claim-tools/src/comp/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Header
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Header;
