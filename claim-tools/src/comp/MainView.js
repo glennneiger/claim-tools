ca3c12d344e2dd7e002c5eef7a9bcb782b920c23
Matt Peters <matthew.george.peters@gmail.com>
2019-03-22 13:08:25 -0400

    added tabs for new or existing claims:

--- claim-tools/src/comp/MainView.js
+++ claim-tools/src/comp/MainView.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import NewClaim from './NewClaim';
 import ExistingClaim from './ExistingClaim';
 import ViewWindow from './ViewWindow';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+
 
 class MainView extends Component {
     state ={
@@ -11,10 +14,17 @@ class MainView extends Component {
     render() {
         return (
             <section className='main-view'>
-                <h1>Main Section</h1>
-                    <NewClaim />
-                    <ExistingClaim />
+                <Tabs defaultActiveKey="newclaim" id="uncontrolled-tab-example">
+                    <Tab eventKey="newclaim" title="New Claim">
+                        <NewClaim />
+                    </Tab>
+                    <Tab eventKey="existingclaim" title="Existing Claim">
+                        <ExistingClaim />
+                    </Tab>
+                </Tabs>;
+                <section className='view-window'>
                     <ViewWindow />
+                </section>
             </section>
         );
     }
