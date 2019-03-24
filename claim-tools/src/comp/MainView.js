ed3c0e2c641208a327735f9a180faea6db56ce73
Matt Peters <matthew.george.peters@gmail.com>
2019-03-24 14:31:09 -0400

    changed UI/UX for under header

--- claim-tools/src/comp/MainView.js
+++ claim-tools/src/comp/MainView.js
@@ -7,6 +7,9 @@ import Tab from 'react-bootstrap/Tab';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 class MainView extends Component {
     state ={
@@ -16,18 +19,24 @@ class MainView extends Component {
     render() {
         return (
             <section className='main-view'>
+            New Claim
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>;
             <Container>
                 <Row>
                     <Col>
                         <section className='claim-window'>
-                            <Tabs defaultActiveKey="newclaim" id="uncontrolled-tab-example">
-                                <Tab eventKey="newclaim" title="New Claim">
-                                    <NewClaim />
-                                </Tab>
-                                <Tab eventKey="existingclaim" title="Existing Claim">
-                                    <ExistingClaim />
-                                </Tab>
-                            </Tabs>
+                            Claim options window
                         </section>
                     </Col>
                     <Col>
