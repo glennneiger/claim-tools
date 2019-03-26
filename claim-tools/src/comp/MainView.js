05446d9c72e2a71c5b635ee2264a8a9409b9ff71
Matt Peters <matthew.george.peters@gmail.com>
2019-03-26 17:38:15 -0400

    made the ins/broker input's pretty

--- claim-tools/src/comp/MainView.js
+++ claim-tools/src/comp/MainView.js
@@ -14,34 +14,32 @@ function NewClaim (props){
         <section>
             <Form>
                 <Form.Row>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>Claim Number</Form.Label>
-                        <Form.Control size='sm' type="text" placeholder="F06..." />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>Policy Number</Form.Label>
-                        <Form.Control size='sm' type="text" placeholder="" />
-                    </Form.Group>
+                    <Col>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Insured Contact</Form.Label>
+                            <Form.Control size='sm' type="text" placeholder="Insured Contact" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Insured Contact Number</Form.Label>
+                            <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
+                        </Form.Group>
+                    </Col>
                 </Form.Row>
                 <Form.Row>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                        <Form.Label>Insured Contact</Form.Label>
-                        <Form.Control size='sm' type="text" placeholder="Insured Contact" />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                        <Form.Label>Insured Contact Number</Form.Label>
-                        <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Broker Contact</Form.Label>
-                    <Form.Control size='sm' type="text" placeholder="Insured Contact" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Broker Contact Number</Form.Label>
-                    <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
-                </Form.Group>
+                    <Col>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Broker Contact</Form.Label>
+                            <Form.Control size='sm' type="text" placeholder="Insured Contact" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Broker Contact Number</Form.Label>
+                            <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
+                        </Form.Group>
+                    </Col>
             </Form.Row>
                 <Form.Group controlId="exampleForm.ControlSelect2">
                     <Form.Label>Insured Address</Form.Label>
