f6d88b29ef22cab33546d7dc445b56d57f5768b0
Matt Peters <matthew.george.peters@gmail.com>
2019-03-26 18:05:17 -0400

    moved NewClaim to its own comp for state

--- claim-tools/src/comp/MainView.js
+++ claim-tools/src/comp/MainView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ExistingClaim from './ExistingClaim';
 import ViewWindow from './ViewWindow';
+import NewClaim from './NewClaim';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Container from 'react-bootstrap/Container';
@@ -8,64 +9,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
-function NewClaim (props){
-    return (
-        <section>
-            <Form>
-                <Form.Row>
-                    <Col>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Insured Contact</Form.Label>
-                            <Form.Control size='sm' type="text" placeholder="Insured Contact" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Insured Contact Number</Form.Label>
-                            <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Row>
-                    <Col>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Broker Contact</Form.Label>
-                            <Form.Control size='sm' type="text" placeholder="Insured Contact" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Broker Contact Number</Form.Label>
-                            <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Row>
-                    <Col>
-                        <Form.Group controlId="exampleForm.ControlSelect2">
-                            <Form.Label>Insured Address</Form.Label>
-                            <Form.Control size='sm' as="textarea" rows='4' placeholder="Insured Address" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group controlId="exampleForm.ControlSelect2">
-                            <Form.Label>Broker Address</Form.Label>
-                            <Form.Control size='sm' as="textarea" rows='4' placeholder="Address" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Group controlId="exampleForm.ControlSelect2">
-                    <Form.Label>Forms</Form.Label>
-                    <Form.Control size='sm' as="textarea" rows='7' placeholder="Forms" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </section>
-    );
-}
 
 class MainView extends Component {
     state ={
--- claim-tools/src/comp/NewClaim.js
+++ claim-tools/src/comp/NewClaim.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 class NewClaim extends Component {
-    render() {
+    state ={
+
+    }
+
+    render () {
         return (
-            <section className='new-claim'>
-                <h1>new claim</h1>
+            <section>
+                <Form>
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Insured Contact</Form.Label>
+                                <Form.Control size='sm' type="text" placeholder="Insured Contact" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Insured Contact Number</Form.Label>
+                                <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Broker Contact</Form.Label>
+                                <Form.Control size='sm' type="text" placeholder="Insured Contact" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Broker Contact Number</Form.Label>
+                                <Form.Control size='sm' type="text" placeholder="numbers only.. we'll format" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect2">
+                                <Form.Label>Insured Address</Form.Label>
+                                <Form.Control size='sm' as="textarea" rows='4' placeholder="Insured Address" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect2">
+                                <Form.Label>Broker Address</Form.Label>
+                                <Form.Control size='sm' as="textarea" rows='4' placeholder="Address" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Group controlId="exampleForm.ControlSelect2">
+                        <Form.Label>Forms</Form.Label>
+                        <Form.Control size='sm' as="textarea" rows='7' placeholder="Forms" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </section>
         );
     }
 }
+
 
 export default NewClaim;
