ef3f29cee27868eb65faf7aa2f1379b5b5cc020a
Matt Peters <matthew.george.peters@gmail.com>
2019-04-03 12:42:12 -0400

    added state to InitialContact

--- claim-tools/src/comp/InitialContact.js
+++ claim-tools/src/comp/InitialContact.js
@@ -1,69 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import ClipIcon from './ClipIcon';
 
 
-function InitialContact (props) {
+class InitialContact extends Component {
+    state ={
 
-    const contactEmailBody = `${ props.claimData ? props.claimData.insuredContact : 'name not found'},
+    }
+
+    contactMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
 Hi Asshole!
 By Asshole!!`;
 
-    return (
-        <Form>
-            <h4>Initial Contact Email</h4>
-            <Form.Group
-                controlId="emailContact">
-                <Form.Label>Email Contact</Form.Label>
-                <Form.Control
-                    size="sm"
-                    type="text"
-                    value={ props.claimData ? props.claimData.insuredEmail : 'No Email Found - Stage Claim'}
+    render(){
+        return (
+            <Form>
+                <h4>Initial Contact Email</h4>
+                <Form.Group
+                    controlId="emailContact">
+                    <Form.Label>Email Contact</Form.Label>
+                    <Form.Control
+                        size="sm"
+                        type="text"
+                        value={ this.props.claimData ? this.props.claimData.insuredEmail : 'No Email Found - Stage Claim'}
+                        />
+                    <ClipIcon
+                        fieldId='emailContact'
                     />
+                </Form.Group>
+                <Form.Group
+                    controlId="emailCarbonCopy">
+                    <Form.Label>Carbon Copies</Form.Label>
+                    <Form.Control
+                        size="sm"
+                        type="text"
+                        value={ `${ this.props.claimData ? this.props.claimData.adjusterEmail : 'No Email Found - Stage Claim' }; ${ this.props.claimData ? this.props.claimData.brokerEmail : 'No Email Found - Stage Claim' }`}
+                        />
+                    <ClipIcon
+                        fieldId='emailCarbonCopy'
+                    />
+                </Form.Group>
+                <Form.Group
+                    controlId="contactEmailSubject">
+                    <Form.Label>Subject</Form.Label>
+                    <Form.Control
+                        size="sm"
+                        type="text"
+                        value={ `Contact Email - GNY Claim: ${ this.props.claimNumber ? this.props.claimNumber : 'Claim Not Found - Stage Claim' } - ${ this.props.claimData ? this.props.claimData.insuredName : 'Insured Name Not Found - Stage Claim' } - Location: ${ this.props.claimData ? this.props.claimData.insuredAddress : 'Adress Not Found - Stage Claim' }`}
+                        />
+                    <ClipIcon
+                        fieldId='contactEmailSubject'
+                    />
+                </Form.Group>
+                <Form.Group
+                    controlId="contactEmailBody">
+                    <Form.Label>Email Body</Form.Label>
+                    <Form.Control
+                        size='sm'
+                        as="textarea"
+                        rows='7'
+                        value={ this.contactMadeEmailBody }
+                        />
+                </Form.Group>
                 <ClipIcon
-                    fieldId='emailContact'
+                    fieldId='contactEmailBody'
                 />
-            </Form.Group>
-            <Form.Group
-                controlId="emailCarbonCopy">
-                <Form.Label>Carbon Copies</Form.Label>
-                <Form.Control
-                    size="sm"
-                    type="text"
-                    value={ `${ props.claimData ? props.claimData.adjusterEmail : 'No Email Found - Stage Claim' }; ${ props.claimData ? props.claimData.brokerEmail : 'No Email Found - Stage Claim' }`}
-                    />
-                <ClipIcon
-                    fieldId='emailCarbonCopy'
-                />
-            </Form.Group>
-            <Form.Group
-                controlId="contactEmailSubject">
-                <Form.Label>Subject</Form.Label>
-                <Form.Control
-                    size="sm"
-                    type="text"
-                    value={ `Contact Email - GNY Claim: ${ props.claimNumber ? props.claimNumber : 'Claim Not Found - Stage Claim' } - ${ props.claimData ? props.claimData.insuredName : 'Insured Name Not Found - Stage Claim' } - Location: ${ props.claimData ? props.claimData.insuredAddress : 'Adress Not Found - Stage Claim' }`}
-                    />
-                <ClipIcon
-                    fieldId='contactEmailSubject'
-                />
-            </Form.Group>
-            <Form.Group
-                controlId="contactEmailBody">
-                <Form.Label>Email Body</Form.Label>
-                <Form.Control
-                    size='sm'
-                    as="textarea"
-                    rows='7'
-                    value={ contactEmailBody }
-                    />
-            </Form.Group>
-            <ClipIcon
-                fieldId='contactEmailBody'
-            />
-        </Form>
-    )
+            </Form>
+        )
+    }
 }
 
 export default InitialContact;
