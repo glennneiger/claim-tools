4b1de090d9eba4efd27c119dc303d2e7fb92b0f5
Matt Peters <matthew.george.peters@gmail.com>
2019-04-03 12:32:06 -0400

    made ClipIcon just an img

--- claim-tools/src/comp/ClipIcon.js
+++ claim-tools/src/comp/ClipIcon.js
@@ -2,17 +2,16 @@ import React from 'react';
 import clipBoard from '../clipboard.svg';
 
 function ClipIcon (props) {
-    function copyField(){
-        // https://www.w3schools.com/howto/howto_js_copy_clipboard.asp 04/02/19
+    function copyField(){// https://www.w3schools.com/howto/howto_js_copy_clipboard.asp 04/02/19
         var copyText = document.getElementById(props.fieldId);
         copyText.select();
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
     };
 
-        return (
-            <img id='clip-board-icon' onClick={copyField} src={clipBoard} alt='clipboard icon'></img>
-        )
+    return (
+        <img id='clip-board-icon' onClick={copyField} src={clipBoard} alt='clipboard icon'></img>
+    )
 }
 
 export default ClipIcon;
