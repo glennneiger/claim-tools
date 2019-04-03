081cba84135e367a9cc2f4c0f63ee3e385214235
Matt Peters <matthew.george.peters@gmail.com>
2019-04-03 13:10:57 -0400

    typed out initial contact text and vars

--- claim-tools/src/comp/InitialContact.js
+++ claim-tools/src/comp/InitialContact.js
@@ -6,12 +6,42 @@ import ClipIcon from './ClipIcon';
 
 class InitialContact extends Component {
     state ={
-
+        firstContactMade: false,
+        firstContactNotMade: false,
+        subroSpoliationLanguage: false,
+        adjusterContactLine: false,
+        requestDocs: false,
+        propertyType: null
     }
 
+    signature = `Thank you,
+
+Matt
+
+Matthew Peters
+Senior Examiner - Property Claims
+Greater New York Mutual Insurance
+200 Madison Avenue - Floor 3
+New York, New York 10016`;
+
+    subroSpoliationLanguage = `We ask that you save any evidence from the loss so we may inspect it for possible subrogation.`;
+
+    adjusterContactLine = `I've also copied the outside adjuster, ${this.props.claimData ? this.props.claimData.adjusterContact : 'NAME NOT FOUND'} (${this.props.claimData ? this.props.claimData.adjusterNumber : 'NUMBER NOT FOUND'}) to this email. If you have not already, please contact them to set up an inspection time`;
+
+    requestDecs = `When you have a moment, could you please forward the ${this.state.propertyType === 'condo' ? 'Condominium By-laws' : 'Proprietary Lease'} for my review?`;
+
+    contactNotMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
+This is Matthew Peters from Greater New York Mutual Insurance, the examiner assigned to this case at the home office. I've left a message for you earlier regarding this loss. When you have a moment, please contact me at the information below so we can discuss the case.
+
+${this.state.subroSpoliationLanguage ? this.subroSpoliationLanguage : ''}
+
+${this.state.adjusterContactLine ? this.adjusterContactLine : ''}
+
+${this.signature}`;
+
     contactMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
-Hi Asshole!
-By Asshole!!`;
+
+This is Matthew Peters from Great New York. It was a pleasure speaking with you earlier. Below is my contact information. Please save it in case you and questions or concerns in the future.`;
 
     render(){
         return (
@@ -60,7 +90,7 @@ By Asshole!!`;
                         size='sm'
                         as="textarea"
                         rows='7'
-                        value={ this.contactMadeEmailBody }
+                        value={ this.contactNotMadeEmailBody }
                         />
                 </Form.Group>
                 <ClipIcon
