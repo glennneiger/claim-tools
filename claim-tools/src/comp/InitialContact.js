e7d32b079e9aabcd897b1ba3992c13dd2517f5c6
Matt Peters <matthew.george.peters@gmail.com>
2019-04-11 19:27:42 -0400

    added button toggle fx

--- claim-tools/src/comp/InitialContact.js
+++ claim-tools/src/comp/InitialContact.js
@@ -6,16 +6,64 @@ import ClipIcon from './ClipIcon';
 
 class InitialContact extends Component {
     state ={
-        contactMade: true,
+        contactMade: false,
         subroSpoliationLanguage: true,
         adjusterContactLine: true,
         requestDocs: true,
         propertyType: null
     }
 
+    toggleContactMade = () => {
+        if(this.state.contactMade === false){
+            this.setState({
+                contactMade: true
+            });
+        } else {
+            this.setState({
+                contactMade: false
+            })
+        }
+    }
+
+    toggleSpoliation = () => {
+        if(this.state.subroSpoliationLanguage === false){
+            this.setState({
+                subroSpoliationLanguage: true
+            });
+        } else {
+            this.setState({
+                subroSpoliationLanguage: false
+            })
+        }
+    }
+
+    toggleAdjusterContact = () => {
+        if(this.state.adjusterContactLine === false){
+            this.setState({
+                adjusterContactLine: true
+            });
+        } else {
+            this.setState({
+                adjusterContactLine: false
+            })
+        }
+    }
+
+    toggleRequestDocs = () => {
+        if(this.state.requestDocs === false){
+            this.setState({
+                requestDocs: true
+            });
+        } else {
+            this.setState({
+                requestDocs: false
+            })
+        }
+    }
+
     render(){
 /* Email Body Optional Text */
-        let signature = `Thank you,
+        const signature = `Thank you,
 
 Matt
 
@@ -25,21 +73,21 @@ Greater New York Mutual Insurance
 200 Madison Avenue - Floor 3
 New York, New York 10016`;
 
-        let subroSpoliationLanguage = `We ask that you save any evidence from the loss so we may inspect it for possible subrogation.
+        const subroSpoliationLanguage = `We ask that you save any evidence from the loss so we may inspect it for possible subrogation.
 
 `;
 
-        let adjusterContactLine = `I've also copied the outside adjuster, ${this.props.claimData ? this.props.claimData.adjusterContact : 'NAME NOT FOUND'} (${this.props.claimData ? this.props.claimData.adjusterNumber : 'NUMBER NOT FOUND'}) to this email. If you have not already, please contact them to set up an inspection time.
+        const adjusterContactLine = `I've also copied the outside adjuster, ${this.props.claimData ? this.props.claimData.adjusterContact : 'NAME NOT FOUND'} (${this.props.claimData ? this.props.claimData.adjusterNumber : 'NUMBER NOT FOUND'}) to this email. If you have not already, please contact them to set up an inspection time.
 
 `;
 
-        let requestDocs = `When you have a moment, could you please forward the ${this.state.propertyType === 'condo' ? 'Condominium By-laws' : 'Proprietary Lease'} for my review?
+        const requestDocs = `When you have a moment, could you please forward the ${this.state.propertyType === 'condo' ? 'Condominium By-laws' : 'Proprietary Lease'} for my review?
 
 `;
 
 /* Contact Not Made Email Body */
 
-        let contactNotMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
+        const contactNotMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
 
 This is Matthew Peters from Greater New York Mutual Insurance, the examiner assigned to this case at the home office. I've left a message for you earlier regarding this loss. When you have a moment, please contact me at the information below so we can discuss the case.
 
@@ -48,7 +96,7 @@ ${signature}`;
 
 /* Contact Made Email Body */
 
-        let contactMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
+        const contactMadeEmailBody = `${ this.props.claimData ? this.props.claimData.insuredContact : 'name not found'},
 
 This is Matthew Peters from Great New York. It was a pleasure speaking with you earlier. Below is my contact information. Please save it in case you and questions or concerns in the future.
 
@@ -97,6 +145,13 @@ ${signature}`;
                 <Form.Group
                     controlId="contactEmailBody">
                     <Form.Label>Email Body</Form.Label>
+                        <Form.Group
+                            controlId='emailBodyOptions'>
+                            <Form.Check inline type="checkbox" label="Contact Made" />
+                            <Form.Check inline type="checkbox" label="Spoliation Language" />
+                            <Form.Check inline type="checkbox" label="Adjuster Contact" />
+                            <Form.Check inline type="checkbox" label="Request Governing Docs" />
+                        </Form.Group>
                     <Form.Control
                         size='sm'
                         as="textarea"
