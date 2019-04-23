08ea03438ab0e70e1a938af56c8fe4cec5832900
Matt Peters <matthew.george.peters@gmail.com>
2019-04-23 17:31:09 -0400

    added formik and yup to settlment comp

--- claim-tools/src/comp/Settlement.js
+++ claim-tools/src/comp/Settlement.js
@@ -9,6 +9,10 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import { Formik } from 'formik';
 import * as yup from 'yup';
+
+const schema = yup.object({
+    claimNumber: yup.string().required(),
+});
 class Settlement extends Component {
     state = {
         rebuildEstimateSource: 'Estimate Source'
@@ -23,45 +27,76 @@ class Settlement extends Component {
     render() {
         return (
             <section className='settlement-view'>
-                <h4>Settlement Options</h4>
-                <InputGroup className="mb-3" size='sm' >
-                    <DropdownButton
-                    as={InputGroup.Prepend}
-                    //variant="outline-secondary"
-                    title={this.state.rebuildEstimateSource}
-                    id="input-group-dropdown-1"
+            <Formik
+                    validationSchema={ schema }
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2)); //alert set to verify text submissions
+                            this.updateCurrentClaimNumber(values.claimNumber);
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                    initialValues={{
+                        claimNumber: 'F0689987'
+                    }}
                 >
-                    <Dropdown.Item as="button" href="#" value='Independent Adjuster' onClick={ this.updateEstimateSource } >Independent Adjuster</Dropdown.Item>
-                    <Dropdown.Item as="button" href="#" value='Builder Consultant' onClick={ this.updateEstimateSource } >Builder Consultant</Dropdown.Item>
-                </DropdownButton>
-                <InputGroup.Prepend>
-                        <InputGroup.Text>RCV</InputGroup.Text>
-                        <InputGroup.Text>$</InputGroup.Text>
-                </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Dollar Amount"
-                        aria-label="Amount (to the nearest dollar)"
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3" size='sm'>
-                    <DropdownButton
-                    as={InputGroup.Prepend}
-                    //variant="outline-secondary"
-                    title={this.state.rebuildEstimateSource}
-                    id="input-group-dropdown-1"
-                >
-                    <Dropdown.Item href="#">Independent Adjuster</Dropdown.Item>
-                    <Dropdown.Item href="#">Builder Consultant</Dropdown.Item>
-                </DropdownButton>
-                <InputGroup.Prepend>
-                        <InputGroup.Text>ACV</InputGroup.Text>
-                        <InputGroup.Text>$</InputGroup.Text>
-                </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Dollar Amount"
-                        aria-label="Amount (to the nearest dollar)"
-                    />
-                </InputGroup>
+                {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    touched,
+                    isValid,
+                    errors,
+                }) => (
+                    <Form
+                        onSubmit={ handleSubmit }
+                    >
+                    <h4>Settlement Options</h4>
+                    <InputGroup className="mb-3" size='sm' >
+                        <DropdownButton
+                        as={InputGroup.Prepend}
+                        //variant="outline-secondary"
+                        title={this.state.rebuildEstimateSource}
+                        id="input-group-dropdown-1"
+                    >
+                        <Dropdown.Item as="button" href="#" value='Independent Adjuster' onClick={ this.updateEstimateSource } >Independent Adjuster</Dropdown.Item>
+                        <Dropdown.Item as="button" href="#" value='Builder Consultant' onClick={ this.updateEstimateSource } >Builder Consultant</Dropdown.Item>
+                    </DropdownButton>
+                    <InputGroup.Prepend>
+                            <InputGroup.Text>RCV</InputGroup.Text>
+                            <InputGroup.Text>$</InputGroup.Text>
+                    </InputGroup.Prepend>
+                        <FormControl
+                            placeholder="Dollar Amount"
+                            aria-label="Amount (to the nearest dollar)"
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3" size='sm'>
+                        <DropdownButton
+                        as={InputGroup.Prepend}
+                        //variant="outline-secondary"
+                        title={this.state.rebuildEstimateSource}
+                        id="input-group-dropdown-1"
+                    >
+                        <Dropdown.Item href="#">Independent Adjuster</Dropdown.Item>
+                        <Dropdown.Item href="#">Builder Consultant</Dropdown.Item>
+                    </DropdownButton>
+                    <InputGroup.Prepend>
+                            <InputGroup.Text>ACV</InputGroup.Text>
+                            <InputGroup.Text>$</InputGroup.Text>
+                    </InputGroup.Prepend>
+                        <FormControl
+                            placeholder="Dollar Amount"
+                            aria-label="Amount (to the nearest dollar)"
+                        />
+                    </InputGroup>
+                        <Button variant="primary" type="submit">
+                            Search
+                        </Button>
+                    </Form>
+            )}
+                </Formik>
             </section>
         );
     }
