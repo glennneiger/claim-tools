8651e73a4745fe427c79c3fb32896ed0e92fe058
Matt Peters <matthew.george.peters@gmail.com>
2019-05-15 12:38:10 -0400

    cleanup up Settlement -- added SettlementEmail

---
+++ claim-tools/src/comp/SettlementEmail.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import ClipIcon from './ClipIcon';
+
+
+function SettlementEmail (props) {
+        return (
+            <section className="settlement-email-view">
+                <Form>
+                    <Form.Group
+                        controlId="contactEmailBody">
+                    <Form.Label>Settlement Email Body</Form.Label>
+                    <Form.Control
+                        size='sm'
+                        as="textarea"
+                        rows='7'
+                        value={ this.state.contactMade ? contactMadeEmailBody : contactNotMadeEmailBody }
+                        />
+                </Form.Group>
+                <ClipIcon
+                    fieldId='contactEmailBody'
+                />
+                </Form>
+            </section>
+        );
+    }
+
+export default SettlementEmail;
