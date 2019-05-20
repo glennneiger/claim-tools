182f38a5cacb64dc3ad60cfe77383a4cf3be743f
Matt Peters <matthew.george.peters@gmail.com>
2019-05-20 13:18:10 -0400

    filling out SettlementEmail comp

--- claim-tools/src/comp/SettlementEmail.js
+++ claim-tools/src/comp/SettlementEmail.js
@@ -4,12 +4,30 @@ import ClipIcon from './ClipIcon';
 
 
 function SettlementEmail (props) {
+    const signature = `Thanks!
+
+    Matt
+    
+    Matthew Peters
+    Senior Examiner - Property Claims
+    Greater New York Mutual Insurance
+    200 Madison Avenue - Floor 3
+    New York, New York 10016`;
+
+
         return (
             <section className="settlement-email-view">
                 <Form>
                     <Form.Group
                         controlId="contactEmailBody">
                     <Form.Label>Settlement Email Body</Form.Label>
+                    <Form.Group
+                        controlId='emailBodyOptions'>
+                        <Form.Check inline type="checkbox" label="Contact Made" />
+                        <Form.Check inline type="checkbox" label="Spoliation Language" />
+                        <Form.Check inline type="checkbox" label="Adjuster Contact" />
+                        <Form.Check inline type="checkbox" label="Request Governing Docs" />
+                    </Form.Group>
                     <Form.Control
                         size='sm'
                         as="textarea"
